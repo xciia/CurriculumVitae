4320e1851926eb5e310d652af75275eebed6597a
Cesar Linux <ariashcesar@gmail.com>
2023-03-08 23:21:47 -0300

    actualizacion del proyecto final

--- JS/function.js
+++ JS/function.js
@@ -2,3 +2,13 @@
 document.getElementById("boton").addEventListener("click", function() {
     document.getElementById("mail").innerHTML = "pauline.hernandez@example.com";
 });
+
+document.getElementById("boton_noche").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#000000";
+    document.body.style.color = "#ffffff"
+});
+
+document.getElementById("boton_dia").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#E5ADA2";
+    document.body.style.color = "#212529"
+});
