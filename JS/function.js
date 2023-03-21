f39d9db70b2eadac823fefebcadaf4eaf7c5a85b
Cesar Linux <ariashcesar@gmail.com>
2023-03-21 13:24:00 -0300

    correciones

--- JS/function.js
+++ JS/function.js
@@ -1,15 +1,16 @@
 
 document.getElementById("boton").addEventListener("click", function() {
-    document.getElementById("mail").innerHTML = '<a href="mailto:pauline.hernandez@example.com" target="_top">pauline.hernandez@example.com</a>';
+    document.getElementById("mail").innerHTML =
+    '<a href="mailto:pauline.hernandez@example.com" target="_top">pauline.hernandez@example.com</a>';
 });
 
 document.getElementById("boton_noche").addEventListener("click", function() {
 
     document.body.style.backgroundColor = "#000000";
-    document.body.style.color = "#ffffff"
+    document.body.style.color = "#ffffff";
 });
 
 document.getElementById("boton_dia").addEventListener("click", function() {
     document.body.style.backgroundColor = "#E5ADA2";
-    document.body.style.color = "#212529"
+    document.body.style.color = "#212529";
 });
